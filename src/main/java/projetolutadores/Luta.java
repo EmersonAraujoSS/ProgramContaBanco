223d41c388926650b2a137a3e80c5c0b742a89ca
emerson.araujo <mcemersonaraujo@gmail.com>
2024-04-30 18:18:03 -0400

    Commit projeto Lutador

---
+++ src/main/java/projetolutadores/Luta.java
@@ -0,0 +1,85 @@
+package projetolutadores;
+
+import java.util.Random;
+
+public class Luta {
+
+    //ATRIBUTOS
+    private Lutador desafiado;  /*USANDO TIPOS ABSTRATOS DE DADOS, ESTÁ ABSTRAINDO DA CLASSE LUTADOR*/
+    private Lutador desafiante;  /*USANDO TIPOS ABSTRATOS DE DADOS, ESTÁ ABSTRAINDO DA CLASSE LUTADOR*/
+    private int rounds;
+    private boolean aprovada;
+
+    //MÉTODOS PÚBLICOS
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2){
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        }else {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
+
+    }
+
+    public void lutar(){
+        if (this.aprovada){
+            System.out.println("### DESAFIADO ###");
+            this.desafiado.apresentar();
+            System.out.println("### DESAFIANTE ###");
+            this.desafiante.apresentar();
+
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); //ele vai gerar 3 resultados 0, 1, 2,
+            switch (vencedor){  //switch:  estrutura de escolha
+                case 0:  //Empate
+
+                    break;
+                case 1:  //Desafiado vence
+
+                    break;
+                case 2:  //Desafiante vence
+
+                    break;
+            }
+
+        }else {
+            System.out.println("A luta não pode acontecer!");
+        }
+    }
+
+    //MÉTODOS ESPECIAIS
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+}
